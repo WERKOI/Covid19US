5c90b95cf403d51a1378a21af5184b16d14ce5c5
WERKOI <korostin04@list.ru>
2022-04-30 02:40:46 +0300

    redact

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,3 +2,25 @@
 	export let segment;
 </script>
 
+<!-- svelte-ignore a11y-no-redundant-roles -->
+<nav class="navbar" role="navigation" aria-label="main navigation">
+	<div class="navbar-brand">
+		<a href="/" class="navbar-item">
+		<img src="logo.png" alt="virus">
+		</a>
+		<span
+		class="navbar-burger"
+		arial-label="menu"
+		aria-expanded="false">
+		<span aria-hidden="true"></span>
+		<span aria-hidden="true"></span>
+		<span aria-hidden="true"></span>
+		</span>
+	</div>
+	<div class="navbar-menu">
+		<div class="navbar-start">
+			<a href="/" class="navbar-item">Home</a>
+			<a href="/about" class="navbar-item">About</a>
+		</div>
+	</div>
+</nav>
