42d2f407b608938267a3ab874fd12616a91ebd92
WERKOI <korostin04@list.ru>
2022-04-30 11:59:40 +0300

    changes

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,25 +2,44 @@
 	export let segment;
 </script>
 
-<!-- svelte-ignore a11y-no-redundant-roles -->
+<style>
+	@media (min-width: 769px) {
+	  nav {
+		border-bottom: solid gray 1px;
+	  }
+	}
+  </style>
+
+
 <nav class="navbar" role="navigation" aria-label="main navigation">
 	<div class="navbar-brand">
-		<a href="/" class="navbar-item">
-		<img src="logo.png" alt="virus">
-		</a>
-		<span
+	  <a rel="prefetch" href="/" class="navbar-item">
+		<img src="logo.jpg" width="32" alt="virus" />
+	  </a>
+	  <span
 		class="navbar-burger"
-		arial-label="menu"
+		aria-label="menu"
 		aria-expanded="false">
-		<span aria-hidden="true"></span>
-		<span aria-hidden="true"></span>
-		<span aria-hidden="true"></span>
-		</span>
+		<span aria-hidden="true" />
+		<span aria-hidden="true" />
+		<span aria-hidden="true" />
+	  </span>
 	</div>
 	<div class="navbar-menu">
-		<div class="navbar-start">
-			<a href="/" class="navbar-item">Home</a>
-			<a href="/about" class="navbar-item">About</a>
-		</div>
+	  <div class="navbar-start">
+		<a
+		  rel="prefetch"
+		  href="/"
+		  class="navbar-item">
+		  Home
+		</a>
+		<a
+		  rel="prefetch"
+		  href="/about"
+		  class="navbar-item">
+		  About
+		</a>
+	  </div>
 	</div>
-</nav>
+  
+  </nav>
